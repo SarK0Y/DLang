0f1ca7b9883beee8e7a4bd2c3d8a0b91bf45ab4e
SarK0Y <sark0y@protonmail.com>
2025-12-01 14:52:22 +0300

    ...

--- source/emp.d
+++ source/emp.d
@@ -1,6 +1,6 @@
 import core.stdc.stdio;
 import std.file;
-import srd.exception;
+import std.exception;
 import gmp_z;
 import std.stdio;
 import std.string;
@@ -9,16 +9,29 @@ extern (C) void EMP (
     ref string file_target,
     ref string Key,
     ref string extra_Key,
+    ref string file_out,
     int word_order = 1,
     int byte_order = 1
 ) {
     auto a = new zz (0);
     string* tst = null;
+    File target, key, fout;
     try {
-        if (exists (file_target) ) {
+        if (!exists (file_target) ) {
+            writeln ("Dear User, file to compress/decode doesn't exist.");
+            return;
         }
+        if (!exists(Key))
+        {
+            writeln("Dear User, Key file for compression/decoding doesn't exist.");
+            return;
+        }
+        target = File (file_target, "rb");
+        key = File (Key, "rb");
+        fout = File (file_out, "w+");
     }
     catch (FileException e ) {
+        writeln ("Failed to prepare files.", e.msg);
     }
 
 

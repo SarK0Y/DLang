6e48a27f905113efc4e7498f1a876f3b538761e4
SarK0Y <sark0y@protonmail.com>
2025-12-17 17:42:48 +0300

    update script

--- source/publish_tam_mods.sh
+++ source/publish_tam_mods.sh
@@ -9,13 +9,23 @@ cp -f ../dub.sdl ./
 git add dub.sdl source
 set m  "-m"
 set count 1
+set indx 0;
 while test "$argv[$count]" = "-m" 
-	set m "$m "\"$argv[(math "$count + 1")]\"
+	if test $count = 1
+            set indx (math "$count + 1")
+            set m "$m \"$argv[$indx]\""
+            echo $count
+	else
+            set indx (math "$count + 1")
+            set m "$m -m \"$argv[$indx]\""
+            echo $count
+	end
 	echo $m
 	set count (math "$count + 2")
 end
 #set final_m (string replace -r '^\"|\"$' '' $m)
-set git_commit "git commit "$m
+set git_commit "git commit $m"
+echo $git_commit
 eval $git_commit
 git push -u
 cd $prev_dir

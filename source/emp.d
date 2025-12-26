b9793f719aef0f1e7ff96bc8c46b9bfc4d695842
SarK0Y <sark0y@protonmail.com>
2025-12-26 17:53:41 +0300

    ...

--- source/emp.d
+++ source/emp.d
@@ -142,7 +142,8 @@ extern (C) ret_emp __emp (zz target, zz key) {
         break;
     }
     map.name = "name1";
-    map.set (0);
+    //map.set (0);
+    map = 0;
     map.prnt;
     simply_print_time ("start tst for __emp");
     for (;;) {
